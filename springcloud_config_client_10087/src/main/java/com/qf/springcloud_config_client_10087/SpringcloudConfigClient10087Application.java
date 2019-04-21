208314dfb00d78e9b9c435e5942e3a3b86a59ace
ken_global <ken_global@qq.com>
2019-04-21 19:54:59 +0800

    OK

--- springcloud_config_client_10087/src/main/java/com/qf/springcloud_config_client_10087/SpringcloudConfigClient10087Application.java
+++ springcloud_config_client_10087/src/main/java/com/qf/springcloud_config_client_10087/SpringcloudConfigClient10087Application.java
@@ -3,6 +3,12 @@ package com.qf.springcloud_config_client_10087;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * bootstrap.yml-系统级的配置文件
+ * application.yml-用户级的配置文件
+ * bootstrap.yml会先与application.yml加载,如果有相同的配置application.yml会覆盖bootstrap.yml
+ */
 @SpringBootApplication
 public class SpringcloudConfigClient10087Application {
 
--- springcloud_zuul_10000/src/main/java/com/qf/springcloud_zuul_10000/filter/MyZuulFilter.java
+++ springcloud_zuul_10000/src/main/java/com/qf/springcloud_zuul_10000/filter/MyZuulFilter.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
+/**
+ * zuul过滤器
+ * 如果请求没有携带一个token参数,这任何请求都不能通过
+ */
 @Component
 public class MyZuulFilter extends ZuulFilter {
     @Override
@@ -18,19 +23,25 @@ public class MyZuulFilter extends ZuulFilter {
 
     @Override
     public int filterOrder() {
+        //数字越大优先级越高
         return 100;
     }
 
     @Override
     public boolean shouldFilter() {
+        //跟根据请求是test1还是test2判断是否过滤 true 过滤 false不过滤
         return true;
     }
 
     @Override
     public Object run() throws ZuulException {
+        //过滤器的具体逻辑
         HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        String requestURI = request.getRequestURI();
+        System.out.println("发送过来的请求:" + requestURI);
         String token = request.getParameter("token");
         if (token == null || !token.equals("admin")){
+            //让zuul将response操作权限放开
             RequestContext.getCurrentContext().setSendZuulResponse(false);
 
             HttpServletResponse response = RequestContext.getCurrentContext().getResponse();
